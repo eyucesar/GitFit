189ac3dc87d681088998c706ea3f7298bbe7f03d
Ebru Yucesar <ebru.yucesar@gmail.com>
2017-06-06 10:00:23 -0500

    test0

--- assets/javascript/exercise.js
+++ assets/javascript/exercise.js
@@ -140,28 +140,17 @@ $(document).ready(function () {
   if (annyang) {
   // Let's define our first command. First the text we expect, and then the function it should call
   var commands = {
-    'Hamstring': function() {
-      muscle = commands[0];
-      for (var i = 0; i < muscleName.length; i++) {
-        if (muscle === muscleName[i]) {
-        //testing
-        console.log("sound test loop");
-        displayExercise(muscle);
-      }
+    'show tps report': function() {
+      $('#tpsreport').animate({bottom: '-100px'});
     }
-  }
+  };
 
   // Add our commands to annyang
   annyang.addCommands(commands);
 
   // Start listening. You can call this here, or attach this call to an event, button, etc.
   annyang.start();
-
 }
-}
-
-
-
 
 });
 
