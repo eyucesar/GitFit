5ce3644c7cb4913c9fde1de9f30f83a8a36553c7
Ebru Yucesar <ebru.yucesar@gmail.com>
2017-06-07 14:03:03 -0500

    test

--- assets/javascript/exercise.js
+++ assets/javascript/exercise.js
@@ -176,6 +176,12 @@ $(document).ready(function () {
       
     }
 
+    // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start();
+
   }
 
 });
