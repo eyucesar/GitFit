0193aaf942dc5ec4775e0feaf825f6fa1b4f35f0
Ebru Yucesar <ebru.yucesar@gmail.com>
2017-06-08 11:25:34 -0500

    heroku test

--- assets/javascript/recipes.js
+++ assets/javascript/recipes.js
@@ -1,5 +1,3 @@
-$(document).ready(function() {
-
 var baseURL = "https://api.edamam.com/search?q="
 var appKey = "4cb43262ed46d6be10b05df6cb89da13"
 var app_ID = "875f4d15"
@@ -18,11 +16,6 @@ function resetSearchResults() {
 //function to create the search query with users input
 function getInput() {
     inputVal = $("#input-field").val().trim();
-    queryURL = baseURL + inputVal + "&limit=" + resultCount + "&app_ID" + app_ID + "&app_key=" + appKey;
-}
-
-function voiceInput() {
-    inputVal = tag;
     queryURL = baseURL + inputVal + "&limit=" + resultCount + "&app_ID" + app_ID + "&app_key=" + appKey;
 }
 
@@ -49,117 +42,74 @@ function showDivs(n) {
     x[cardIndex].style.display = "block";
 }
 
-
 //On click event to get recipes and list them to the recipesRow div
-
+$(document).ready(function() {
     event.preventDefault();
 
-    $("button").on("click", displayRecipes);
+    $("button").on("click", function(event) {
+        event.preventDefault();
+        getInput();
+        $("#recipesRow").empty();
 
-    function ajaxCall () {
         $.ajax({
-        url: queryURL,
-        method: "GET"
+            url: queryURL,
+            method: "GET"
             // Store all of the retrieved data inside of an object called "response"
-    }).done(function(response) {
-        if (response.hits.length === 0) return;
+        }).done(function(response) {
+            if (response.hits.length === 0) return;
 
-        // Log the queryURL
-        var resultsElem, image, container, label, calories, link;
-        for (var i = 0; i < Math.max(8, response.hits.length); i++) {
-            recipeURL = response.hits[i].recipe.url;
+            // Log the queryURL
+            var resultsElem, image, container, label, calories, link;
+            for (var i = 0; i < Math.max(8, response.hits.length); i++) {
+                recipeURL = response.hits[i].recipe.url;
 
-            resultsElem = $("<div>");
-            resultsElem.addClass("card");
-            if (i === 0) {
-                resultsElem.show();
-            } else {
-                resultsElem.hide();
+                resultsElem = $("<div>");
+                resultsElem.addClass("card");
+                if (i === 0) {
+                    resultsElem.show();
+                } else {
+                    resultsElem.hide();
+                }
+
+                image = $("<img>");
+                image.addClass("image");
+                image.attr("src", response.hits[i].recipe.image);
+                image.attr("alt", "Img");
+                resultsElem.append(image);
+
+                container = $("<div>");
+                container.addClass("recipeContainer");
+                resultsElem.append(container);
+
+                label = $("<p>");
+                label.text(response.hits[i].recipe.label);
+                label.addClass("label");
+                container.append(label);
+                // console.log("i= " + i + " label= " + response.hits[i].recipe.label);
+                calories = $("<p>Calories: " + Math.round(response.hits[i].recipe.calories) + "</p>");
+                calories.addClass("calories");
+                container.append(calories);
+
+                link = $("<a>");
+                link.attr("href", response.hits[i].recipe.url);
+                link.attr("target", "_blank");
+
+                link.addClass("link");
+                link.text(response.hits[i].recipe.url)
+                container.append(link);
+
+                // add cards to recipes row
+                $("#recipesRow").append(resultsElem);
+
+                // add buttons
+                buttonLeft = $("<button class='w3-button w3-black w3-display-left' onclick='showDivs(-1)'>&#10094</button>");
+                $("#recipesRow").append(buttonLeft);
+
+                buttonRight = $("<button class='w3-button w3-black w3-display-right' onclick='showDivs(1)'>&#10095;</button>");
+                $("#recipesRow").append(buttonRight);
+
             }
-
-            image = $("<img>");
-            image.addClass("image");
-            image.attr("src", response.hits[i].recipe.image);
-            image.attr("alt", "Img");
-            resultsElem.append(image);
-
-
-            container = $("<div>");
-            container.addClass("recipeContainer");
-            resultsElem.append(container);
-
-
-            label = $("<p>");
-            label.text(response.hits[i].recipe.label);
-            label.addClass("label");
-            container.append(label);
-
-            // console.log("i= " + i + " label= " + response.hits[i].recipe.label);
-
-            calories = $("<p>Calories: " + Math.round(response.hits[i].recipe.calories) + "</p>");
-            calories.addClass("calories");
-            container.append(calories);
-
-            link = $("<a>");
-            link.attr("href", response.hits[i].recipe.url);
-            link.attr("target", "_blank");
-
-            link.addClass("link");
-            link.text(response.hits[i].recipe.url)
-            container.append(link);
-
-            // add cards to recipes row
-            $("#recipesRow").append(resultsElem);
-
-            // add buttons
-
-            buttonLeft = $("<button class='w3-button w3-black w3-display-left' onclick='showDivs(-1)'>&#10094</button>");
-            $("#recipesRow").append(buttonLeft);
-
-            buttonRight = $("<button class='w3-button w3-black w3-display-right' onclick='showDivs(1)'>&#10095;</button>");
-            $("#recipesRow").append(buttonRight);
-
-        }
-
-    });
-    }
-
-
-function displayRecipes() {
-    event.preventDefault();
-    getInput();
-    $("#recipesRow").empty();
-    ajaxCall();
-    resetSearchResults();
-};
-
-function voiceRecipes() {
-    voiceInput();
-    $("#recipesRow").empty();
-    ajaxCall();
-    resetSearchResults();
-};
-
-
-//voice detection code starts here
-if (annyang) {
-
-    // defining commands
-    var commands = {
-        'recipes with *tag': function(tag) {
-            voiceRecipes();
-            console.log(tag);
-        } 
-    };
-
-
-}
-
-    // Add our commands to annyang
-  annyang.addCommands(commands);
-
-  // Start listening. You can call this here, or attach this call to an event, button, etc.
-  annyang.start();
-
-
+        });
+        resetSearchResults();
+    })
 });
